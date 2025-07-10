f2cf9631f85bba108ebd5c350a263785710361f2
Jenny O'Halloran <jenny.ohalloran@cranfield.ac.uk>
2025-07-10 13:11:37 +0100

    Profile & Settings GUI

--- tpp_vis_app/src/app/profile/profile.ts
+++ tpp_vis_app/src/app/profile/profile.ts
@@ -7,6 +7,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+
 
 
 
@@ -16,23 +19,56 @@ import { AuthService } from '../services/auth.service';
   imports: [MatTabsModule,
             MatIconModule,
             MatMenuModule,
-            CommonModule],
+            CommonModule,
+          FormsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
 export class Profile {
 
   currentView: string = 'profile';
-
+  fontSizes: number[] = [12, 14, 16, 18, 20, 24, 28, 32];
+  fontSize: number = 16;
   constructor(private backendApiService: backendApiService,
               private dataTransferService: DataTransferService,
               private authService: AuthService,
               private router: Router) {}
 
-  
+  colorThemes = [
+    { name: 'Light', background: '#ffffff', text: '#000000' },
+    { name: 'Dark', background: '#1e1e1e', text: '#ffffff' },
+    { name: 'Ocean', background: '#e0f7fa', text: '#006064' },
+    { name: 'Sunset', background: '#ffccbc', text: '#4e342e' },
+    { name: 'Mint', background: '#e0ffe0', text: '#004d00' }
+  ];
+
+  palettes = [
+    { name: 'Warm', colors: ['#ff6f61', '#ffb74d', '#ffe082'] },
+    { name: 'Cool', colors: ['#4fc3f7', '#81d4fa', '#b3e5fc'] },
+    { name: 'Earthy', colors: ['#a1887f', '#8d6e63', '#bcaaa4'] },
+    { name: 'Vibrant', colors: ['#e91e63', '#9c27b0', '#673ab7'] },
+    { name: 'Pastel', colors: ['#f8bbd0', '#e1bee7', '#d1c4e9'] }
+  ];
+
+  selectedPalette = this.palettes[0]; // default
+
+  selectedTheme2 = this.colorThemes[0];
   
   currentUser = '';
   isHelpOpen = false;
+
+
+  allPrivileges = [
+    'Perform Analysis',
+    'Upload Pedigree Data',
+    'Upload Environmental Data',
+    'Upload Genomic Data',
+    'Edit Users',
+    'Manage Settings',
+    'View Analytics',
+    'Delete Content'
+  ];
+
 
   async ngOnInit() {
 
@@ -45,8 +81,21 @@ export class Profile {
       this.router.navigate(['/login'])
 
     }
+  }
 
+  selectedPrivileges: string[] = [];
 
+  togglePrivilege(privilege: string) {
+    const index = this.selectedPrivileges.indexOf(privilege);
+    if (index === -1) {
+      this.selectedPrivileges.push(privilege);
+    } else {
+      this.selectedPrivileges.splice(index, 1);
+    }
+  }
+
+  isSelected(privilege: string): boolean {
+    return this.selectedPrivileges.includes(privilege);
   }
   
   toggleHelpPanel() {
