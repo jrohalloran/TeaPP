c4ab4fd3b088c85dc5a2b195bd849a978db2470d
Jenny O'Halloran <jenny.ohalloran@cranfield.ac.uk>
2025-07-23 16:29:08 +0100

    small

--- tpp_vis_app/back_end/controllers/scripts/perform_kinship.R
+++ tpp_vis_app/back_end/controllers/scripts/perform_kinship.R
@@ -92,27 +92,27 @@ gp <- create.gpData(pedigree=ped)
 summary(gp)
 
 message("Performing Kinship Matrix")
-#kinship.mx <- kin(gp)
+kinship.mx <- kin(gp)
 
 
-#file<- paste0(temp_dir,"/kinship_matrix.txt")
+file<- paste0(temp_dir,"/kinship_matrix.txt")
 #write.csv(kinship.mx, file, row.names = TRUE)
 
-#write.table(kinship.mx, file, sep = "\t", quote = FALSE, row.names = TRUE, col.names = NA)
+write.table(kinship.mx, file, sep = "\t", quote = FALSE, row.names = TRUE, col.names = NA)
 
 #library(AGHmatrix)
 
-pedigree <- gp$pedigree
+#pedigree <- gp$pedigree
 
-pedigree <- data.frame(
-  ID = pedigree$ID,
-  Par1 = pedigree$Par1,
-  Par2 = pedigree$Par2
-)
+#pedigree <- data.frame(
+  #ID = pedigree$ID,
+  #Par1 = pedigree$Par1,
+  #Par2 = pedigree$Par2
+#)
 
 
 # Compute additive kinship matrix
-A_matrix <- Amatrix(pedigree, ploidy = 2, dominance = FALSE)
+#A_matrix <- Amatrix(pedigree, ploidy = 2, dominance = FALSE)
 ## Tea Trees are Diploid
 ## A Matrix - pedigree-based 
 
@@ -124,5 +124,5 @@ A_matrix <- Amatrix(pedigree, ploidy = 2, dominance = FALSE)
 
 ##write.table(A_matrix, "A_matrix.txt")
 
-file<- paste0(temp_dir,"/kinship_matrix.txt")
-write.table(A_matrix, file, sep = "\t", quote = FALSE, row.names = TRUE, col.names = NA)
+#file<- paste0(temp_dir,"/kinship_matrix.txt")
+#write.table(A_matrix, file, sep = "\t", quote = FALSE, row.names = TRUE, col.names = NA)
